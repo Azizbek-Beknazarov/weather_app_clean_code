3dda226bdf98f032722341c6ee0a071aa5455271
Azizbek-Beknazarov <61731420+Azizbek-Beknazarov@users.noreply.github.com>
2022-11-21 20:39:39 +0500

    Update constants.dart

--- lib/core/util/constants.dart
+++ lib/core/util/constants.dart
@@ -2,7 +2,7 @@ class Constants {
   static const String SERVER_XATO_XABAR = 'Shahar topilmadi';
   static const String INTERNET_XATO_XABAR = 'Internet ulanmagan';
   static const String INVALID_INPUT_XATO_XABAR = 'Shahar nomi noto\'g\'ri';
-  static const String apiKey = '9cfa3dd1b766c77679f13a1dc437c15b';
+  static const String apiKey = 'your key';
   static const String obhavoUrl =
       'http://api.openweathermap.org/data/2.5/weather?';
   static const String units = '&units=metric';
